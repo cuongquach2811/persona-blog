408d08b1e41b88cce2669b1cdd7d47dee0dce529
cuongquach2811 <me@quachcuong.com>
2024-11-26 16:51:41 +0700

    (fix) fix existing sourcecode can run

--- src/pages/posts/[post].tsx
+++ src/pages/posts/[post].tsx
@@ -1,15 +1,14 @@
 import { GetStaticProps, GetStaticPaths } from "next";
-import renderToString from "next-mdx-remote/render-to-string";
-import { MdxRemote } from "next-mdx-remote/types";
-import hydrate from "next-mdx-remote/hydrate";
+import { serialize } from "next-mdx-remote/serialize";
+import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
 import matter from "gray-matter";
 import { fetchPostContent } from "../../lib/posts";
 import fs from "fs";
 import yaml from "js-yaml";
-import { parseISO } from 'date-fns';
+import { parseISO } from "date-fns";
 import PostLayout from "../../components/PostLayout";
 
-import InstagramEmbed from "react-instagram-embed";
+import { InstagramEmbed } from "react-social-media-embed";
 import YouTube from "react-youtube";
 import { TwitterTweetEmbed } from "react-twitter-embed";
 
@@ -20,13 +19,14 @@ export type Props = {
   tags: string[];
   author: string;
   description?: string;
-  source: MdxRemote.Source;
+  source: MDXRemoteSerializeResult;
 };
 
 const components = { InstagramEmbed, YouTube, TwitterTweetEmbed };
-const slugToPostContent = (postContents => {
-  let hash = {}
-  postContents.forEach(it => hash[it.slug] = it)
+
+const slugToPostContent = ((postContents) => {
+  let hash: Record<string, any> = {};
+  postContents.forEach((it) => (hash[it.slug] = it));
   return hash;
 })(fetchPostContent());
 
@@ -39,7 +39,6 @@ export default function Post({
   description = "",
   source,
 }: Props) {
-  const content = hydrate(source, { components })
   return (
     <PostLayout
       title={title}
@@ -49,13 +48,13 @@ export default function Post({
       author={author}
       description={description}
     >
-      {content}
+      <MDXRemote {...source} components={components} />
     </PostLayout>
-  )
+  );
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const paths = fetchPostContent().map(it => "/posts/" + it.slug);
+  const paths = fetchPostContent().map((it) => ({ params: { post: it.slug } }));
   return {
     paths,
     fallback: false,
@@ -63,12 +62,17 @@ export const getStaticPaths: GetStaticPaths = async () => {
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const slug = params.post as string;
+  const slug = params?.post as string;
   const source = fs.readFileSync(slugToPostContent[slug].fullPath, "utf8");
   const { content, data } = matter(source, {
-    engines: { yaml: (s) => yaml.load(s, { schema: yaml.JSON_SCHEMA }) as object }
+    engines: { yaml: (s) => yaml.load(s, { schema: yaml.JSON_SCHEMA }) as object },
   });
-  const mdxSource = await renderToString(content, { components, scope: data });
+
+  // Serialize MDX content
+  const mdxSource = await serialize(content, {
+    scope: data, // Pass variables into the MDX content
+  });
+
   return {
     props: {
       title: data.title,
@@ -77,8 +81,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
       description: "",
       tags: data.tags,
       author: data.author,
-      source: mdxSource
+      source: mdxSource,
     },
   };
 };
-
